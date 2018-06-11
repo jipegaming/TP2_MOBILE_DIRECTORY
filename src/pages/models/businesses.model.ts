e612a726a3e4c74cba6d45a31b99bdc7beff24e4
jipegaming <jpalvarez.tcc@gmail.com>
2018-06-10 21:13:12 -1000

    import api

---
+++ src/pages/models/businesses.model.ts
@@ -0,0 +1,11 @@
+import { Skills } from './skills.model';
+
+export class Businesses {
+    id: string;
+    name: string;
+    logo: string;
+    latitude: string;
+    longitude: string;
+    abus: string;
+    skills: Skills;
+}
